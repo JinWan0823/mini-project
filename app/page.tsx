21322f6203cd447d9327bce51055eeeaff68ba0d
jinWan0823 <dhksl823@gmail.com>
2025-11-06 11:02:59 +0900

    🎉 Next.js 15 + React 19 기반 “댓글 피드 미니 프로젝트”

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="font-sans dark:bg-black">
+      <main className="max-w-[1100px] w-[95%] h-[600px] mx-auto bg-[#333]"></main>
+    </div>
+  );
+}
