e0f433834ae387de2ec6ea859edeca2f57fe6aef
jinWan0823 <dhksl823@gmail.com>
2025-11-06 12:55:31 +0900

    💄 댓글 폼 ui 제작

--- app/components/CommentForm.tsx
+++ app/components/CommentForm.tsx
@@ -1,11 +1,30 @@
 export default function CommentForm() {
   return (
     <div
-      className="comment-form absolute bottom-4 left-1/2 -translate-x-1/2
-      bg-[#dfdfdf] w-full p-2 rounded-10 shadow-xl
-    "
+      className="comment-form
+      w-full p-2 bg-white relative"
     >
-      ddd
+      <div
+        className="absolute top-0 left-0 w-full h-[30px] -translate-y-[100%]
+      bg-gradient-to-b from-transparent via-white/10 to-white"
+      ></div>
+      <div className="w-full bg-[#dfdfdf] shadow-xl py-4 px-2">
+        <input
+          type="text"
+          placeholder="이름을 입력해주세요."
+          className="bg-white p-1"
+        />
+        <textarea
+          className="w-full max-h-[110px] h-[90px] bg-white mt-2 p-1"
+          placeholder="내용을 입력해주세요."
+        />
+        <button
+          type="submit"
+          className="font-bold w-[120px] h-[42px] rounded text-white p-1 bg-blue-400 cursor-pointer"
+        >
+          작성하기
+        </button>
+      </div>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ import Header from "./components/Header";
 export default function Home() {
   return (
     <div className="font-sans dark:bg-black">
-      <main className="max-w-[1100px] w-[95%] py-2 h-screen min-h-[400px] mx-auto relative ">
-        <div className="flex flex-col h-full overflow-hidden border-1 bg-[#333] rounded-[12px] overflow-hidden">
+      <main className="max-w-[1100px] w-[95%] py-2 h-screen min-h-[400px] mx-auto">
+        <div className="flex flex-col w-full relative h-full overflow-hidden border-1 bg-[#333] rounded-[12px] overflow-hidden">
           <Header />
           <ul className="flex-1 overflow-y-auto px-2">
             <CommentLi />
@@ -16,9 +16,13 @@ export default function Home() {
             <CommentLi />
             <CommentLi />
             <CommentLi />
+            <CommentLi />
+            <CommentLi />
+            <CommentLi />
+            <CommentLi />
           </ul>
+          <CommentForm />
         </div>
-        <CommentForm />
       </main>
     </div>
   );
