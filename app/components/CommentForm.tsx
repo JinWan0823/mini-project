f738cf95f4f437da6ae7432a908b9363e2232ed5
jinWan0823 <dhksl823@gmail.com>
2025-11-06 11:35:58 +0900

    💄 미니 댓글 폼 layout 구성

---
+++ app/components/CommentForm.tsx
@@ -0,0 +1,11 @@
+export default function CommentForm() {
+  return (
+    <div
+      className="comment-form absolute bottom-4 left-1/2 -translate-x-1/2
+      bg-[#dfdfdf] w-full p-2 rounded-10 shadow-xl
+    "
+    >
+      ddd
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
+import CommentForm from "./components/CommentForm";
+import CommentLi from "./components/CommnetLi";
+import Header from "./components/Header";
+
 export default function Home() {
   return (
     <div className="font-sans dark:bg-black">
-      <main className="max-w-[1100px] w-[95%] h-[600px] mx-auto bg-[#333]"></main>
+      <main className="max-w-[1100px] w-[95%] py-2 h-screen min-h-[400px] mx-auto relative ">
+        <div className="flex flex-col h-full overflow-hidden border-1 bg-[#333] rounded-[12px] overflow-hidden">
+          <Header />
+          <ul className="flex-1 overflow-y-auto px-2">
+            <CommentLi />
+          </ul>
+        </div>
+        <CommentForm />
+      </main>
     </div>
   );
 }
